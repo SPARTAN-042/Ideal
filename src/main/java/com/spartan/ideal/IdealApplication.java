77429de174303420fcd7cf9aa48b4528ee4ec0b3
Clint Le Grange <sentinal042@gmail.com>
2024-01-14 22:29:05 +0000

    Removing start method - yet to be redundant

--- src/main/java/com/spartan/ideal/IdealApplication.java
+++ src/main/java/com/spartan/ideal/IdealApplication.java
@@ -2,20 +2,10 @@ package com.spartan.ideal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Scanner;
 @SpringBootApplication
 public class IdealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IdealApplication.class, args);
-	}
-
-	public static void start() {
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Enter product name: ");
-
-		String itemName = input.nextLine();
 	}
 }
