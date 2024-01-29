5c9dd94507684ce7b6bd80d74aec9dccd4813050
Clint Le Grange <sentinal042@gmail.com>
2024-01-29 21:02:25 +0000

    Refactored SeleniumService.java

--- src/main/java/com/spartan/ideal/service/SeleniumService.java
+++ src/main/java/com/spartan/ideal/service/SeleniumService.java
@@ -1,18 +1,31 @@
 package com.spartan.ideal.service;
 
 import com.spartan.ideal.SiteConnector;
+import com.spartan.ideal.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SeleniumService {
 
-    @Autowired
-    private SiteConnector siteConnector;
 
-    public void runSelenium(String itemName) {
-        siteConnector.callAmazon(itemName);
-        siteConnector.callEbay(itemName);
-        siteConnector.callOnbuy(itemName);
+    private final SiteConnector siteConnector;
+
+    @Autowired
+    public SeleniumService(SiteConnector siteConnector) {
+        this.siteConnector = siteConnector;
     }
+
+   public List<Product> runSelenium(String itemName) {
+
+       List<Product> products = new ArrayList<>();
+       products.addAll(siteConnector.callAmazon(itemName));
+       products.addAll(siteConnector.callEbay(itemName));
+       products.addAll(siteConnector.callOnbuy(itemName));
+
+       return products;
+   }
 }
