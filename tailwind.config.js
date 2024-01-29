a940e08aaf9ecac05f5b069c0358153f26c2c62f
Clint Le Grange <sentinal042@gmail.com>
2024-01-29 21:03:21 +0000

    Extended font families and updated content purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,23 @@
 module.exports = {
   mode: process.env.NODE_ENV ? 'jit' : undefined,
   content: [
-    "./src/**/*.{html,js}",
+    "./src/**/*.html",
+    "./src/**/*.js",
   ],
   safelist: {
     deep: [/^dark:/],
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        header: ['Montserrat', 'sans-serif'],
+        body: ['Roboto', 'sans-serif'],
+        accent: ['Lato', 'sans-serif'],
+        logo: ['Exo', 'sans-serif'],
+        secondary: ['"Open Sans"', 'sans-serif'],
+        call_to_action: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extends: {},
