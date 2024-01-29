3a2926ca7aa4eb557db6496b3903c2c9cbadafbf
Clint Le Grange <sentinal042@gmail.com>
2024-01-29 21:02:47 +0000

    Completed scraping methods for sites

--- src/main/java/com/spartan/ideal/SiteConnector.java
+++ src/main/java/com/spartan/ideal/SiteConnector.java
@@ -1,5 +1,6 @@
 package com.spartan.ideal;
 
+import com.spartan.ideal.model.Product;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -36,130 +38,115 @@ public class SiteConnector {
         driver.findElement(By.xpath(searchButtonXpath)).click();
     }
 
-    public void callAmazon(String itemName)  {
-        String ebayUrl = "https://www.amazon.co.uk/";
+    public List<Product> callAmazon(String itemName)  {
+        String amazonUrl = "https://www.amazon.co.uk/";
         String searchBoxXpath = "//*[@id=\"twotabsearchtextbox\"]";
         String searchButtonXpath = "//*[@id=\"nav-search-submit-button\"]";
 
-        performSearch(ebayUrl, searchBoxXpath, searchButtonXpath, itemName);
+        performSearch(amazonUrl, searchBoxXpath, searchButtonXpath, itemName);
 
-        List<String> productName = new ArrayList<>();
-        List<String> productImage = new ArrayList<>();
-        List<String> productPrice = new ArrayList<>();
-        List<String> productLink = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
         List<WebElement> items = driver.findElements(By.xpath("//div[contains(@class, 's-result-item s-asin')]"));
 
-        WebElement img;
-        WebElement name;
-        List<WebElement> wholePrice;
-        List<WebElement> fractionPrice;
-        String price;
-        WebElement link;
 
         for (WebElement item : items) {
+            Product product = new Product();
 
-            img = item.findElement(By.xpath(".//img[@class = 's-image']"));
-            productImage.add(img.getAttribute("src"));
+            product.setImgUrl(item.findElement(By.xpath(".//img[@class = 's-image']")).getAttribute("src"));
 
-            name = item.findElement(By.xpath(".//span[contains(@class, 'a-color-base a-text-normal')]"));
-            productName.add(name.getText());
+            product.setProductName(item.findElement(By.xpath(".//span[contains(@class, 'a-color-base a-text-normal')]")).getText());
 
-            wholePrice = item.findElements(By.xpath(".//span[@class='a-price-whole']"));
-            fractionPrice = item.findElements(By.xpath(".//span[@class='a-price-fraction']"));
+            List<WebElement> wholePrices = item.findElements(By.xpath(".//span[@class='a-price-whole']"));
+            List<WebElement> fractionPrices = item.findElements(By.xpath(".//span[@class='a-price-fraction']"));
 
-            if (!wholePrice.isEmpty() && !fractionPrice.isEmpty()) {
-                price = String.join(".",wholePrice.get(0).getText(), fractionPrice.get(0).getText());
+            if (!wholePrices.isEmpty() && !fractionPrices.isEmpty()) {
+                String tempPrice = String.join(".", wholePrices.get(0).getText(), fractionPrices.get(0).getText());
+                String priceWithoutSymbol = tempPrice.startsWith(".") ? "0" + tempPrice : tempPrice;
+                product.setPrice(priceWithoutSymbol);
             } else {
-                price = "0";
-
+                product.setPrice("0");
             }
-            productPrice.add(price);
 
+            product.setProductLink(item.findElement(By.xpath(".//a[@class = 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']")).getAttribute("href"));
 
-            link = item.findElement(By.xpath(".//a[@class = 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']"));
-            productLink.add(link.getAttribute("href"));
-
+            products.add(product);
         }
 
-        System.out.printf("Image link: %s\nProduct name: %s\nProduct price: %s\nProduct link: %s\n", productImage, productName, productPrice, productLink);
+        return products;
     }
 
-    public void callEbay(String itemName) {
+    public List<Product> callEbay(String itemName) {
         String ebayUrl = "https://www.ebay.co.uk/";
         String searchBoxXpath = "//*[@id=\"gh-ac\"]";
         String searchButtonXpath = "//*[@id=\"gh-btn\"]";
 
         performSearch(ebayUrl, searchBoxXpath, searchButtonXpath, itemName);
 
-        List<String> productName = new ArrayList<>();
-        List<String> productImage = new ArrayList<>();
-        List<String> productPrice = new ArrayList<>();
-        List<String> productLink = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
-        List<WebElement> items = driver.findElements(By.xpath("//div[contains(@class, 's-item__wrapper clearfix')"));
-
-        WebElement img;
-        WebElement name;
-        List<WebElement> wholePrice;
-        List<WebElement> fractionPrice;
-        String price;
-        WebElement link;
+        List<WebElement> items = driver.findElements(By.xpath("//div[contains(@class, 's-item__wrapper clearfix')]"));
 
         for (WebElement item : items) {
+            Product product = new Product();
 
-            img = item.findElement(By.xpath(".//div[class=\"s-item__image-wrapper image-treatment\""));
-            productImage.add(img.getAttribute(""));
+            product.setImgUrl(item.findElement(By.xpath(".//div[@class='s-item__image']/a/div/img")).getAttribute("src"));
 
-            name = item.findElement(By.xpath(""));
-            productName.add(name.getText());
+            product.setProductName(item.findElement(By.className("s-item__title")).getText());
 
-            wholePrice = item.findElements(By.xpath(""));
-            fractionPrice = item.findElements(By.xpath(""));
+            List<WebElement> prices = item.findElements(By.cssSelector(".s-item__price"));
 
+            if (!prices.isEmpty()) {
+                String tempPrice = prices.get(0).getText();
+                if (tempPrice.length() > 1) {
+                    String priceWithoutSymbol = tempPrice.substring(1);
+                    product.setPrice(priceWithoutSymbol);
+                } else {
+                    product.setPrice("0");
+                }
+            } else {
+                product.setPrice("0");
+            }
+
+            product.setProductLink(item.findElement(By.xpath(".//a[@class='s-item__link']")).getAttribute("href"));
+
+            products.add(product);
         }
 
-        System.out.printf("eBay - Image link: %s\nProduct name: %s\nProduct price: %s\nProduct link: %s\n", productImage, productName, productPrice, productLink);
+        return products;
     }
 
-    public void callOnbuy(String itemName) {
+    public List<Product> callOnbuy(String itemName) {
         String onBuyUrl = "https://www.onbuy.com/gb/";
-        String searchBoxXpath = "";
-        String searchButtonXpath = "";
+        String searchBoxXpath = "//*[@id=\"search\"]/fieldset/input";
+        String searchButtonXpath = "//*[@id=\"search\"]/fieldset/button";
 
         performSearch(onBuyUrl, searchBoxXpath, searchButtonXpath, itemName);
 
-        List<String> productName = new ArrayList<>();
-        List<String> productImage = new ArrayList<>();
-        List<String> productPrice = new ArrayList<>();
-        List<String> productLink = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
-        List<WebElement> items = driver.findElements(By.xpath(""));
-
-        WebElement img;
-        WebElement name;
-        List<WebElement> wholePrice;
-        List<WebElement> fractionPrice;
-        String price;
-        WebElement link;
+        List<WebElement> items = driver.findElements(By.className("product"));
 
         for (WebElement item : items) {
+            Product product = new Product();
 
-            img = item.findElement(By.xpath(""));
-            productImage.add(img.getAttribute(""));
+            product.setImgUrl(item.findElement(By.xpath("//span[contains(@class, 'image')]/picture/img")).getAttribute("src"));
 
-            name = item.findElement(By.xpath(""));
-            productName.add(name.getText());
+            product.setProductName(item.findElement(By.className("name")).getText());
 
-            wholePrice = item.findElements(By.xpath(""));
-            fractionPrice = item.findElements(By.xpath(""));
+            List<WebElement> prices = item.findElements(By.className("value"));
 
+            if (!prices.isEmpty()) {
+                String priceWithoutSymbol = prices.get(0).getText().substring(1);
+                product.setPrice(priceWithoutSymbol);
+            } else {
+                product.setPrice("0");
+            }
 
-            link = item.findElement(By.xpath(".//a[@class = 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']"));
-            productLink.add(link.getAttribute("href"));
+            product.setProductLink(item.findElement(By.xpath("//div[contains(@class, 'product cb')]/a")).getAttribute("href"));
 
+            products.add(product);
         }
-
-        System.out.printf("OnBuy - Image link: %s\nProduct name: %s\nProduct price: %s\nProduct link: %s\n", productImage, productName, productPrice, productLink);
+        return products;
     }
 }
