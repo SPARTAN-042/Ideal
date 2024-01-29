8a0c9772e7ec150c64098e8a7df84449bcfed2d7
Clint Le Grange <sentinal042@gmail.com>
2024-01-29 20:59:48 +0000

    Refactoring SeleniumController.java

--- src/main/java/com/spartan/ideal/controller/SeleniumController.java
+++ src/main/java/com/spartan/ideal/controller/SeleniumController.java
@@ -1,21 +1,32 @@
 package com.spartan.ideal.controller;
 
+import com.spartan.ideal.model.Product;
 import com.spartan.ideal.service.SeleniumService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
 public class SeleniumController {
 
+    private final SeleniumService seleniumService;
+
     @Autowired
-    private SeleniumService seleniumService;
+    public SeleniumController(SeleniumService seleniumService) {
+        this.seleniumService = seleniumService;
+    }
 
     @PostMapping("/run-selenium")
-    public ResponseEntity<String> runSelenium(@RequestParam String itemName) {
-        seleniumService.runSelenium(itemName);
-        return ResponseEntity.ok("Selenium run successful!");
+    public String search(@RequestParam String itemName, Model model) {
+        List<Product> products = seleniumService.runSelenium(itemName);
+        model.addAttribute("products", products);
+        return "search-results";
     }
 }
