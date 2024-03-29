583d93b98e7b13383c205fee73a8e1487eab3f62
Clint Le Grange <sentinal042@gmail.com>
2024-03-29 18:11:54 +0000

    - Introduced additional JPA annotations for TrackedItem - Added LocalDateTime attribute - Included setters for Price, Product and User

--- src/main/java/com/spartan/ideal/model/TrackedItem.java
+++ src/main/java/com/spartan/ideal/model/TrackedItem.java
@@ -2,20 +2,26 @@ package com.spartan.ideal.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
+@Table(name = "tracked_item")
 public class TrackedItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trackedItemId")
     private Long trackedItemId;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
     private Product product;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
+
+    private LocalDateTime lastUpdated;
 
     public Long getTrackedItemId() {
         return trackedItemId;
@@ -23,5 +29,14 @@ public class TrackedItem {
 
     public void setTrackedItemId(Long trackedItemId) {
         this.trackedItemId = trackedItemId;
+    }
+
+    public void setPrice(String price) {
+    }
+
+    public void setProduct(Product product) {
+    }
+
+    public void setUser(User user) {
     }
 }
