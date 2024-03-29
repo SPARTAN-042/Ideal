7c8c3ca88294b95165137c4533d6b8839b3d761d
Clint Le Grange <sentinal042@gmail.com>
2024-03-29 15:47:21 +0000

    Commented on the functionality of the app entry point

--- src/main/java/com/spartan/ideal/IdealApplication.java
+++ src/main/java/com/spartan/ideal/IdealApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+/* The @SpringBootApplication annotation is condensed full of other annotations for configuration
+	These include:
+	@Configuration
+	@EnableAutoConfiguration
+	@ComponentScan
+ */
 @SpringBootApplication
 @EntityScan(basePackages = "com.spartan.ideal")
 public class IdealApplication {
 
+	//The entry point for the program
 	public static void main(String[] args) {
 		SpringApplication.run(IdealApplication.class, args);
 	}
