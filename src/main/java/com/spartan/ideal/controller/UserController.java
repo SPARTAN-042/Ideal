45d660452211e076420b2d676740652acfb30873
Clint Le Grange <sentinal042@gmail.com>
2024-03-29 18:19:19 +0000

    - Reduced UserController to lone endpoint

--- src/main/java/com/spartan/ideal/controller/UserController.java
+++ src/main/java/com/spartan/ideal/controller/UserController.java
@@ -1,44 +1,10 @@
 package com.spartan.ideal.controller;
 
-import com.spartan.ideal.model.User;
-import com.spartan.ideal.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping
-    public String getAllUsers(Model model) {
-        List<User> users = userService.getAllUsers();
-        model.addAttribute("users", users);
-        return "user-list";
-    }
-
-    @GetMapping("/{id}")
-    public String getUserById(@PathVariable Long id, Model model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        return "user-details";
-    }
-
-    @PostMapping
-    public String saveUser(@ModelAttribute User user) {
-        userService.saveUser(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return "redirect:/users";
-    }
 }
